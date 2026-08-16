4ef8f8c4a46107e1f3af2d35a851b6f1989201e4
Frontasts <steamfronts0110@gmail.com>
2026-08-16 23:47:13 +0300

    Добавлен enum Volume требующийся по условиям задачи

---
+++ HomeWorckManagingSoundLevels/Volume.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckManagingSoundLevels
+{
+    public enum Volume
+    {
+        Mute,
+        Low,
+        Medium,
+        High
+    }
+}

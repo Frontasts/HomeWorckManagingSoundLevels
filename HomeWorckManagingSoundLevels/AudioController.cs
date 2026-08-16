6182766a8e3585d517e8b994659b07f5f893f157
Frontasts <steamfronts0110@gmail.com>
2026-08-16 23:48:08 +0300

    Добавлен аудио контролер для переключения звука, требуется по задачи

---
+++ HomeWorckManagingSoundLevels/AudioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorckManagingSoundLevels
+{
+    public class AudioController
+    {
+        private Volume _currentVolume;
+
+        public AudioController(Volume initialVolume)
+        {
+            _currentVolume = initialVolume;
+        }
+    }
+}
